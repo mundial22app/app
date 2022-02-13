4361bcb301c8e7caa9f024e7e6634bf7b3d956f5
ostwi <jakub.ostwald@gmail.com>
2022-02-13 21:42:59 +0100

    Initial commit with experimental authorization

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { Auth } from '@supabase/ui'
+import { supabase } from '../lib/initSupabase'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Auth.UserContextProvider supabaseClient={supabase}>
+      <Component {...pageProps} />
+    </Auth.UserContextProvider>
+  )
+}
+
+export default MyApp
